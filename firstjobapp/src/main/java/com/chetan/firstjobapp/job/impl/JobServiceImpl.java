ad73aef7e76d656913a592401ab9cf8e6c1dfb53
cdepanagoudra <cdepanagoudra@gmail.com>
2024-11-27 12:07:00 +0530

    initial commit

---
+++ firstjobapp/src/main/java/com/chetan/firstjobapp/job/impl/JobServiceImpl.java
@@ -0,0 +1,23 @@
+package com.chetan.firstjobapp.job.impl;
+
+import com.chetan.firstjobapp.job.Job;
+import com.chetan.firstjobapp.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class JobServiceImpl implements JobService {
+    private List<Job> jobs = new ArrayList<>();
+    private Long nextId = 1L;
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        job.setId(nextId++);
+        jobs.add(job);
+    }
+}
---
+++ firstjobapp/src/test/java/com/chetan/firstjobapp/FirstjobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chetan.firstjobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
