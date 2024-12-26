c8a3ed834c7a9cdf795c88c32429a016240c4180
cdepanagoudra <cdepanagoudra@gmail.com>
2024-12-26 21:54:40 +0530

    updated gateway

--- gateway/src/main/java/com/microservices/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/microservices/gateway/GatewayApplication.java
@@ -2,12 +2,25 @@ package com.microservices.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GatewayApplication.class, args);
+
+		// Manually calling the method
+		GatewayApplication app = new GatewayApplication();
+		ResponseEntity<String> response = app.hellowWorld();
+		System.out.println(response.getBody());
+	}
+	public ResponseEntity<String> hellowWorld() {
+		String s = "chetan";
+		return ResponseEntity.ok("Hello, " + s + "!");
 	}
 
 }
